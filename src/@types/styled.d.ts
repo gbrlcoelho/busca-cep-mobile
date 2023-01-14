bcdff75cc4923434c2a76afa59dc2a68704b3c99
gbrlcoelho <coelhogabriel67@gmail.com>
2023-01-14 15:50:44 -0300

    feat(styles): created app theme

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,4 +1,4 @@
-import {theme} from '@styles/Theme'
+import {theme} from '@styles/index'
 import 'styled-components'
 
 declare module 'styled-components' {
---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+import {normalize} from '@utils/index'
+
+export const theme = {
+  colors: {
+    white: '#fff',
+    background: '#282c34',
+    yellow: '#f1c400',
+  },
+  fontSize: {
+    h1: normalize(24),
+    h2: normalize(16),
+    h3: normalize(14),
+    h4: normalize(12),
+    h5: normalize(10),
+    h6: normalize(8),
+  },
+}
