32c47aea2649e89bcf367d7ce2ed2b301d83698e
gbrlcoelho <coelhogabriel67@gmail.com>
2023-01-14 16:27:51 -0300

    refactor(theme): added new properties to app theme

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -5,6 +5,9 @@ export const theme = {
     white: '#fff',
     background: '#282c34',
     yellow: '#f1c400',
+    darkGray: '#353535',
+    disabled: '#EEEEEE',
+    middleGray: '#5A5A5A',
   },
   fontSize: {
     h1: normalize(24),
@@ -13,5 +16,17 @@ export const theme = {
     h4: normalize(12),
     h5: normalize(10),
     h6: normalize(8),
+  },
+
+  buttonSize: {
+    height: {
+      small: normalize(26),
+      large: normalize(40),
+    },
+
+    width: {
+      small: normalize(88),
+      large: normalize(240),
+    },
   },
 }
