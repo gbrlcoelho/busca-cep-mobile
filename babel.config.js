a9ab1b015155cb51e32a872dfdf979d3b75a8893
gbrlcoelho <coelhogabriel67@gmail.com>
2023-01-14 11:48:27 -0300

    chore(global): added react-native-dotenv

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,14 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+    [
       'module-resolver',
       {
         root: ['./src'],
